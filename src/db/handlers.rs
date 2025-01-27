b9599131c08c689b167fa7a0d6bbf54ce36ebee3
Neko Nik <nikhil@nekonik.com>
2025-01-27 14:57:03 +0530

    Moved queries to no verified queries

--- src/db/handlers.rs
+++ src/db/handlers.rs
@@ -1,6 +1,8 @@
 use actix_web::{get, post, web, HttpResponse, Responder};
 use serde::{Deserialize, Serialize};
 use crate::db::state::PostgresState;
+use sqlx::Row;
+
 
 #[derive(Deserialize, Serialize)]
 pub struct Note {
@@ -21,15 +23,14 @@ pub async fn create_note_handler(
     body: web::Json<Note>,         // The request body
     state: web::Data<PostgresState>, // The state containing the DB pool
 ) -> impl Responder {
-    // Insert query with placeholders for $1, $2
-    let result = sqlx::query!(
+    let result = sqlx::query(
         r#"
         INSERT INTO notes (title, content)
         VALUES ($1, $2)
-        "#,
-        body.title,
-        body.content
+        "#
     )
+    .bind(&body.title)
+    .bind(&body.content)
     .execute(&state.db_pool)
     .await
     .map_err(|err| err.to_string());
@@ -43,16 +44,22 @@ pub async fn create_note_handler(
 // GET /postgres/notes
 #[get("/notes")]
 pub async fn list_notes_handler(state: web::Data<PostgresState>) -> impl Responder {
-    // Fetch all notes from the database
-    let result = sqlx::query_as!(
-        Record,  // Return type of the query
-        "SELECT id, title, content FROM notes"
+    let result = sqlx::query(
+        r#"
+        SELECT id, title, content FROM notes
+        "#
     )
+    .map(|row: sqlx::postgres::PgRow| Record {
+        id: row.get("id"),
+        title: row.get("title"),
+        content: row.get("content"),
+    })
     .fetch_all(&state.db_pool)
-    .await;
+    .await
+    .map_err(|err| err.to_string());
 
     match result {
-        Ok(notes) => HttpResponse::Ok().json(notes), // Return as JSON
+        Ok(notes) => HttpResponse::Ok().json(notes),
         Err(err) => HttpResponse::InternalServerError().json(format!("Failed: {}", err)),
     }
 }
