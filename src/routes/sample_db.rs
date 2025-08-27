3389ebdf1974a9a3d8b968afb2b52b61d7294170
Neko Nik <nikhil@nekonik.com>
2025-08-27 12:53:23 +0530

    Refactor note handlers to improve caching and fix variable naming

--- src/routes/sample_db.rs
+++ src/routes/sample_db.rs
@@ -4,12 +4,25 @@ use crate::types::{AppCache, make_key};
 use deadpool_postgres::Pool as PgPool;
 
 
+
+async fn cache_data<T, S>(key: S, value: &T, cache: &AppCache)
+    where
+        T: serde::Serialize + serde::de::DeserializeOwned + Send + Sync + 'static,
+        S: Into<String>
+{
+    // Serialize the value to a JSON string
+    let json = serde_json::to_string(value).unwrap();
+
+    // Insert the JSON string into the cache
+    cache.insert(make_key(key), json).await;
+}
+
 #[post("/create-note")]
 pub async fn create_note_handler(
     body: web::Json<Note>,         // The request body
-    state: web::Data<PgPool>, // The state containing the DB pool
+    pg_pool: web::Data<PgPool>, // The state containing the DB pool
 ) -> impl Responder {
-    match add_new_notes(&state, vec![body.into_inner()]).await {
+    match add_new_notes(&pg_pool, vec![body.into_inner()]).await {
         Ok(_) => HttpResponse::Ok().json("Note created successfully!"),
         Err(err) => HttpResponse::ExpectationFailed().json(format!("Failed: {}", err)),
     }
@@ -17,9 +30,24 @@ pub async fn create_note_handler(
 
 
 #[get("/notes")]
-pub async fn list_notes_handler(state: web::Data<PgPool>) -> impl Responder {
-    match fetch_all_notes(&state).await {
-        Ok(notes) => HttpResponse::Ok().json(notes),
+pub async fn list_notes_handler(
+    pg_pool: web::Data<PgPool>,
+    cache: web::Data<AppCache>
+) -> impl Responder {
+    let cache_key = "list_notes_handler";
+
+    // If cache is present then serve from cache
+    if let Some(cached_notes) = cache.get(&make_key(cache_key)).await {
+        return HttpResponse::Ok()
+            .insert_header(("X-Cache", "HIT"))
+            .json(serde_json::from_str::<Vec<Note>>(&cached_notes).unwrap());
+    }
+
+    match fetch_all_notes(&pg_pool).await {
+        Ok(notes) => {
+            cache_data(cache_key, &notes, &cache).await;
+            HttpResponse::Ok().json(notes)
+        }
         Err(_) => HttpResponse::ExpectationFailed().finish(),
     }
 }
@@ -33,7 +61,7 @@ pub async fn create_session_handler(
     // Generate a new session ID
     let session_id = uuid::Uuid::new_v4().to_string();
 
-    state.insert(make_key(session_id.clone()), body.clone()).await;
+    state.insert(make_key(session_id.clone()), body).await;
 
     HttpResponse::Ok().body(session_id)
 }
